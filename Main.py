112e5fa4861f61fc780ffa809565d39d1291a3dd
jwp8807 <jesster_07@hotmail.com>
2015-02-10 18:58:26 -0600

    Created webpage opening and hyperlinks

--- Main.py
+++ Main.py
@@ -14,53 +14,76 @@ class Main:
         #sets window to master, sets title, and window size
         self.master = master
         self.master.title("Encyclopedia App")
-        self.master.geometry("180x200")
+        self.master.geometry("500x230")
 
         #Creates labels, buttons, and textbox
-        lblTitle = Label(self.master, text="Encyclopedia", font=("Purisa", 12, "bold"), fg="green")
-        lblSearch = Label(self.master, text="Search: ")
-        txtBoxSearch = Entry(self.master, textvariable=userSearch)
-        btnSearch = Button(self.master, text="Search", width=10, command=self.search)
-        btnQuit = Button(self.master, text="Close", width=10, command=self.close)
+        lblTitle = Label(self.master, text="Searchster", font=("Times 16 bold"), fg="green", )
+        lblSearch = Label(self.master, text="Search: ", font=("Times 10"))
+        txtBoxSearch = Entry(self.master, width=17, textvariable=userSearch)
+        btnSearch = Button(self.master, text="Search", width=14, command=self.search)
+        btnQuit = Button(self.master, text="Close", width=14, command=self.close)
+        lblWikiLabel = Label(self.master, text="           Wikipedia URL", font=("Times 10 bold"))
+        lblFlickrLabel = Label(self.master, text="            Flickr URL", font=("Times 10 bold"))
+        lblTwitterLabel = Label(self.master, text="          Twitter URL", font=("Times 10 bold"))
 
         #Places labels, buttons, and textbox in grid format
-        lblTitle.grid(row=1, columnspan=3)
-        lblSearch.grid(row=2, column=1, sticky=W)
+        lblTitle.grid(row=1, column=2)
+        lblSearch.grid(row=2, column=1, sticky=E)
         txtBoxSearch.grid(row=2, column=2)
-        btnSearch.grid(row=3, columnspan=3)
-        btnQuit.grid(row=4, columnspan=3)
+        btnSearch.grid(row=3, column=2)
+        btnQuit.grid(row=4, column=2)
+        lblWikiLabel.grid(row=1, column=4, sticky=W)
+        lblFlickrLabel.grid(row=3, column=4, sticky=W)
+        lblTwitterLabel.grid(row=5, column=4, sticky=W)
+
+
+    #Wikipedia Callback Event
+    def wikicallback(self, event):
+        #Opens the hyperlink when left-clicked on
+        webbrowser.open("http://en.wikipedia.org/w/index.php?title=" + str(userSearch))
+
+    #Flickr Callback Event
+    def flickrcallback(self, event):
+        #Opens the hyperlink when left-clicked on
+        webbrowser.open("https://www.flickr.com/search/?q=" + str(userSearch))
+
+    #Twitter Callback Event
+    def twittercallback(self, event):
+        #Opens the hyperlink when left-clicked on
+         webbrowser.open("https://twitter.com/search?q=" + str(userSearch) + "&src=typd")
 
 
     #Search Function
     def search(self):
+        #Gets text from search textbox
         global userSearch
         userSearch = userSearch.get()
 
-        webbrowser.open("http://en.wikipedia.org/w/index.php?title=" +userSearch)
+        #Opens the webbrowsers
+        webbrowser.open("http://en.wikipedia.org/w/index.php?title=" + userSearch)
+        webbrowser.open("https://www.flickr.com/search/?q=" + userSearch)
+        webbrowser.open("https://twitter.com/search?q=" + userSearch + "&src=typd")
+
+        #Displays Wikipedia hyperlink in label and binds it to left-click event and places in grid
+        lblDisplayWikiURL = Label(self.master, text="http://en.wikipedia.org/w/index.php?title=" + userSearch, fg="Blue", cursor="hand2")
+        lblDisplayWikiURL.bind('<Button-1>', self.wikicallback)
+        lblDisplayWikiURL.grid(row=2, column=4, sticky=W)
+
+        #Displays Flickr hyperlink in label and binds it to left-click event and places in grid
+        lblDisplayFlickrURL = Label(self.master, text="https://www.flickr.com/search/?q=" + userSearch, fg="Blue", cursor="hand2")
+        lblDisplayFlickrURL.bind('<Button-1>', self.flickrcallback)
+        lblDisplayFlickrURL.grid(row=4, column=4, sticky=W)
+
+        #Displays Twitter hyperlink in label and binds it to left-click event and places in grid
+        lblDisplayTwitterURL = Label(self.master, text="https://twitter.com/search?q=" + userSearch + "&src=typd", fg="Blue", cursor="hand2")
+        lblDisplayTwitterURL.bind('<Button-1>', self.twittercallback)
+        lblDisplayTwitterURL.grid(row=6, column=4, sticky=W)
+
 
 
     #Function for closing the window
     def close(self):
         self.master.destroy()
-
-
-
-
-
-
-
-
-
-
-
-
-# opener = urllib.request
-# opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-# infile = opener.urlopen("http://en.wikipedia.org/w/index.php?title=" +userSearch)
-# page = infile.read()
-
-
-
 
 
 
